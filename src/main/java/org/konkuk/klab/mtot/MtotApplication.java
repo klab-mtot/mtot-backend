02af0e7e363e113c2f21246e09434fa781e67c50
donghoony <aru0504@naver.com>
2023-05-07 21:19:42 +0900

    feat: 최초 환경세팅

---
+++ src/main/java/org/konkuk/klab/mtot/MtotApplication.java
@@ -0,0 +1,13 @@
+package org.konkuk.klab.mtot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MtotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MtotApplication.class, args);
+	}
+
+}
