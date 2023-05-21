feee9062d75c04c5f55cf867c9b931630ec54158
algoORgoal <qufstar46@gmail.com>
2023-05-21 23:06:45 +0900

    feat: disallow access to endpoints while not signed in

---
+++ src/main/java/org/konkuk/klab/mtot/config/SecurityConfiguration.java
@@ -0,0 +1,30 @@
+package org.konkuk.klab.mtot.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.WebSecurityConfigurer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests((authorize) -> authorize
+                        .requestMatchers(HttpMethod.GET,"/members").permitAll()
+                        .anyRequest().authenticated()
+                );
+        return http.build();
+    }
+}
