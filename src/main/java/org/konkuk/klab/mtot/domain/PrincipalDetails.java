be0d9d01ccc7d62cdd091b1ca6fac076421622e4
algoORgoal <qufstar46@gmail.com>
2023-05-25 22:44:38 +0900

    feat: add Authentication related objects

---
+++ src/main/java/org/konkuk/klab/mtot/domain/PrincipalDetails.java
@@ -0,0 +1,63 @@
+package org.konkuk.klab.mtot.domain;
+
+import com.google.api.client.googleapis.auth.oauth2.GoogleIdToken;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
+
+import static java.util.Map.entry;
+
+
+@Getter
+@RequiredArgsConstructor
+public class PrincipalDetails implements UserDetails {
+
+    private final Member member;
+
+    @Override
+    public String getPassword() {
+        return member.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return member.getEmail();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        List<GrantedAuthority> authorityList = AuthorityUtils.createAuthorityList("ROLE_USER");
+        return authorityList;
+    }
+}
