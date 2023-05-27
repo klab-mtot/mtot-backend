6b8605db43c4e28560e7f46d6c8e27f2da07f81d
GwonPyo <ngp9441@gmail.com>
2023-05-27 10:05:18 +0900

    add Query annotation to delete friendship

--- src/main/java/org/konkuk/klab/mtot/repository/FriendshipRepository.java
+++ src/main/java/org/konkuk/klab/mtot/repository/FriendshipRepository.java
@@ -2,6 +2,9 @@ package org.konkuk.klab.mtot.repository;
 
 import org.konkuk.klab.mtot.domain.Friendship;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
@@ -11,6 +14,8 @@ public interface FriendshipRepository extends JpaRepository<Friendship, Long> {
     Optional<Friendship> findByRequesterIdAndReceiverId(Long requesterId, Long receiverId);
 
     @Transactional
-    int deleteByRequesterIdAndReceiverId(Long requesterId, Long receiverId);
+    @Modifying
+    @Query("delete from Friendship f where f.requester.id=:requesterId and f.receiver.id=:receiverId")
+    int deleteByRequesterIdAndReceiverId(@Param("requesterId") Long requesterId, @Param("receiverId") Long receiverId);
 }
 
