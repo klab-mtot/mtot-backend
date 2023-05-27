192761d3c42dbb032f3eb661ec2ea19e5cdefb7b
donghoony <aru0504@naver.com>
2023-05-27 17:28:59 +0900

    feat: 중간 테이블 MemberTeam에 접근 시 memberId 파라미터 제거 (토큰 기반 LoginMemberEmail 사용)에 따른 repository email 기반 검색 추가, Controller API 변경

--- src/main/java/org/konkuk/klab/mtot/service/MemberTeamService.java
+++ src/main/java/org/konkuk/klab/mtot/service/MemberTeamService.java
@@ -1,20 +1,20 @@
 package org.konkuk.klab.mtot.service;
 
 import lombok.RequiredArgsConstructor;
-import org.konkuk.klab.mtot.domain.Team;
 import org.konkuk.klab.mtot.domain.Member;
 import org.konkuk.klab.mtot.domain.MemberTeam;
+import org.konkuk.klab.mtot.domain.Team;
 import org.konkuk.klab.mtot.dto.response.MemberTeamGetAllResponse;
 import org.konkuk.klab.mtot.dto.response.MemberTeamGetResponse;
 import org.konkuk.klab.mtot.dto.response.MemberTeamJoinResponse;
-import org.konkuk.klab.mtot.dto.request.MemberTeamJoinRequest;
-import org.konkuk.klab.mtot.repository.TeamRepository;
-import org.konkuk.klab.mtot.repository.MemberTeamRepository;
 import org.konkuk.klab.mtot.repository.MemberRepository;
+import org.konkuk.klab.mtot.repository.MemberTeamRepository;
+import org.konkuk.klab.mtot.repository.TeamRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.Objects;
 
 @Service
 @RequiredArgsConstructor
@@ -24,13 +24,16 @@ public class MemberTeamService {
     private final MemberRepository memberRepository;
     private final TeamRepository teamRepository;
     @Transactional
-    public MemberTeamJoinResponse registerMemberToTeam(MemberTeamJoinRequest request){
-        Member member = memberRepository.findById(request.getMemberId())
+    public MemberTeamJoinResponse registerMemberToTeam(String leaderEmail, Long teamId, Long memberId){
+        Member member = memberRepository.findById(memberId)
                 .orElseThrow(()-> new RuntimeException("ID에 해당하는 멤버를 찾을 수 없습니다."));
-        Team team = teamRepository.findById(request.getTeamId())
+        Team team = teamRepository.findById(teamId)
                 .orElseThrow(()-> new RuntimeException("ID에 해당하는 그룹를 찾을 수 없습니다."));
+        Member leader = memberRepository.findByEmail(leaderEmail)
+                .orElseThrow(()-> new RuntimeException("로그인 유저를 찾을 수 없습니다."));
+        if (!Objects.equals(team.getLeaderId(), leader.getId())) throw new RuntimeException("해당 유저는 리더가 아닙니다.");
 
-        memberTeamRepository.findByMemberIdAndTeamId(request.getMemberId(), request.getTeamId())
+        memberTeamRepository.findByMemberIdAndTeamId(teamId, memberId)
                 .ifPresent(memberTeam -> {throw new RuntimeException("그룹에 이미 존재하는 멤버입니다.");});
 
         memberTeamRepository.save(new MemberTeam(member, team));
@@ -38,8 +41,8 @@ public class MemberTeamService {
     }
 
     @Transactional(readOnly = true)
-    public MemberTeamGetAllResponse getMemberTeamsByMemberId(Long memberId){
-        List<MemberTeam> memberTeams = memberTeamRepository.findAllByMemberId(memberId);
+    public MemberTeamGetAllResponse getMemberTeamsByMemberEmail(String email){
+        List<MemberTeam> memberTeams = memberTeamRepository.findAllByMemberEmail(email);
         return new MemberTeamGetAllResponse(memberTeams.stream()
                 .map(memberTeam -> new MemberTeamGetResponse(memberTeam.getMember().getId(), memberTeam.getTeam().getId()))
                 .toList());
