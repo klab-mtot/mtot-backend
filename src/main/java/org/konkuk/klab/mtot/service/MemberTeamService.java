70fae03bfdc18f69605cd2bb44f4a0e507a11954
donghoony <aru0504@naver.com>
2023-05-27 22:12:10 +0900

    feat: Replace RuntimeException to custom exceptions

--- src/main/java/org/konkuk/klab/mtot/service/MemberTeamService.java
+++ src/main/java/org/konkuk/klab/mtot/service/MemberTeamService.java
@@ -1,16 +1,19 @@
 package org.konkuk.klab.mtot.service;
 
 import lombok.RequiredArgsConstructor;
-import org.konkuk.klab.mtot.domain.Team;
 import org.konkuk.klab.mtot.domain.Member;
 import org.konkuk.klab.mtot.domain.MemberTeam;
+import org.konkuk.klab.mtot.domain.Team;
+import org.konkuk.klab.mtot.dto.request.MemberTeamJoinRequest;
 import org.konkuk.klab.mtot.dto.response.MemberTeamGetAllResponse;
 import org.konkuk.klab.mtot.dto.response.MemberTeamGetResponse;
 import org.konkuk.klab.mtot.dto.response.MemberTeamJoinResponse;
-import org.konkuk.klab.mtot.dto.request.MemberTeamJoinRequest;
-import org.konkuk.klab.mtot.repository.TeamRepository;
-import org.konkuk.klab.mtot.repository.MemberTeamRepository;
+import org.konkuk.klab.mtot.exception.DuplicateMemberOnTeamException;
+import org.konkuk.klab.mtot.exception.MemberNotFoundException;
+import org.konkuk.klab.mtot.exception.TeamNotFoundException;
 import org.konkuk.klab.mtot.repository.MemberRepository;
+import org.konkuk.klab.mtot.repository.MemberTeamRepository;
+import org.konkuk.klab.mtot.repository.TeamRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -26,12 +29,14 @@ public class MemberTeamService {
     @Transactional
     public MemberTeamJoinResponse registerMemberToTeam(MemberTeamJoinRequest request){
         Member member = memberRepository.findById(request.getMemberId())
-                .orElseThrow(()-> new RuntimeException("ID에 해당하는 멤버를 찾을 수 없습니다."));
+                .orElseThrow(MemberNotFoundException::new);
         Team team = teamRepository.findById(request.getTeamId())
-                .orElseThrow(()-> new RuntimeException("ID에 해당하는 그룹를 찾을 수 없습니다."));
+                .orElseThrow(TeamNotFoundException::new);
 
         memberTeamRepository.findByMemberIdAndTeamId(request.getMemberId(), request.getTeamId())
-                .ifPresent(memberTeam -> {throw new RuntimeException("그룹에 이미 존재하는 멤버입니다.");});
+                .ifPresent(memberTeam -> {
+                    throw new DuplicateMemberOnTeamException();
+                });
 
         memberTeamRepository.save(new MemberTeam(member, team));
         return new MemberTeamJoinResponse(team.getId());
