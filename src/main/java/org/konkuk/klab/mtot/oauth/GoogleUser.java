a395bcb6802b674f37f83ea6cd5ae90b46093577
donghoony <aru0504@naver.com>
2023-05-27 23:23:34 +0900

    feat: GoogleUser Builder 추가

--- src/main/java/org/konkuk/klab/mtot/oauth/GoogleUser.java
+++ src/main/java/org/konkuk/klab/mtot/oauth/GoogleUser.java
@@ -1,5 +1,6 @@
 package org.konkuk.klab.mtot.oauth;
 
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -14,4 +15,10 @@ public class GoogleUser {
     private String family_name;
     private String picture;
     private String locale;
+    @Builder
+    public GoogleUser(String email, String name, String picture){
+        this.email = email;
+        this.name = name;
+        this.picture = picture;
+    }
 }
