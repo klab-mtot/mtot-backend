7358c7e6243ecdb2f1dcec1db984a07d8a23a4bf
donghoony <aru0504@naver.com>
2023-05-28 16:56:03 +0900

    feat: Journey 수정, 기본 Post과의 연관 구현

--- src/main/java/org/konkuk/klab/mtot/domain/Journey.java
+++ src/main/java/org/konkuk/klab/mtot/domain/Journey.java
@@ -1,45 +1,34 @@
 package org.konkuk.klab.mtot.domain;
 
 import jakarta.persistence.*;
-import lombok.*;
-
-import java.util.ArrayList;
-import java.util.List;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Journey {
+
     @Id @GeneratedValue()
     @Column(name = "journey_id")
     private Long id;
 
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Team team;
+
     @Column(name = "name", nullable = false)
     private String name;
 
-    @Column(name = "team_id")
-    private Long team_id;//many to one
+    @OneToOne(mappedBy = "journey")
+    private Post post;
 
-    @Column(name = "post_id")
-    private Long post_id;//one to one
-
-    @OneToMany(mappedBy = "journey")
-    private List<Photo> photos = new ArrayList<>();
-
-    @OneToMany(mappedBy = "journey")
-    private List<Locations> locations = new ArrayList<>();
-
-    public Journey(String name, Long team_id) {
-        this.team_id = team_id;
-        this.name = name;
-        this.post_id = new Post().getId();
+    public Journey(Team team, String journeyName) {
+        this.name = journeyName;
+        this.team = team;
+        team.getJourneyList().add(this);
     }
-    public void setPost(long post_id){
-        this.post_id = post_id;
+    public void setPost(Post post) {
+        this.post = post;
     }
-
-
 }
-
-
-
