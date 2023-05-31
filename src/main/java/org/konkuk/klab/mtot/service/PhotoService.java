27f5046eecfd731192a2224db40b6bd363980ade
donghoony <aru0504@naver.com>
2023-05-31 22:49:08 +0900

    feat: Photo 구현 및 filePath 추가

---
+++ src/main/java/org/konkuk/klab/mtot/service/PhotoService.java
@@ -0,0 +1,60 @@
+package org.konkuk.klab.mtot.service;
+
+import lombok.RequiredArgsConstructor;
+import org.konkuk.klab.mtot.domain.Journey;
+import org.konkuk.klab.mtot.domain.Photo;
+import org.konkuk.klab.mtot.domain.Pin;
+import org.konkuk.klab.mtot.dto.response.PhotoUploadResponse;
+import org.konkuk.klab.mtot.exception.JourneyNotFoundException;
+import org.konkuk.klab.mtot.exception.PinNotFound;
+import org.konkuk.klab.mtot.exception.TeamAccessDeniedException;
+import org.konkuk.klab.mtot.repository.JourneyRepository;
+import org.konkuk.klab.mtot.repository.PhotoRepository;
+import org.konkuk.klab.mtot.repository.PinRepository;
+import org.konkuk.klab.mtot.s3.AwsS3FileSupporter;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class PhotoService {
+
+    private final AwsS3FileSupporter awsS3FileSupporter;
+    private final PhotoRepository photoRepository;
+    private final JourneyRepository journeyRepository;
+    public final PinRepository pinRepository;
+
+    @Transactional
+    public PhotoUploadResponse uploadPhotos(String loginEmail, Long pinId, List<MultipartFile> multipartFiles) throws IOException {
+
+        Pin pin = pinRepository.findById(pinId)
+                .orElseThrow(PinNotFound::new);
+        Long journeyId = pin.getJourney().getId();
+
+        Journey journey = journeyRepository.findById(journeyId)
+                .orElseThrow(JourneyNotFoundException::new);
+
+        journey.getTeam().getMemberTeams()
+                .stream()
+                .filter(memberTeam -> memberTeam.getMember().getEmail().equals(loginEmail))
+                .findAny()
+                .orElseThrow(TeamAccessDeniedException::new);
+
+        List<Long> photoIds = new ArrayList<>();
+        List<String> filePaths = awsS3FileSupporter.uploadImages(multipartFiles);
+
+        for(String filePath : filePaths){
+            Photo photo = new Photo(pin, filePath, LocalDate.now());
+            Long photoId = photoRepository.save(photo).getId();
+            photoIds.add(photoId);
+        }
+        return new PhotoUploadResponse(photoIds, filePaths);
+    }
+
+}
