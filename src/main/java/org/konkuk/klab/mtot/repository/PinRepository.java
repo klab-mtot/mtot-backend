517706c7ea1c9998f730a69c539cf82744691380
donghoony <aru0504@naver.com>
2023-05-31 22:47:52 +0900

    feat: Pin 구현 (Journey 1 : N Pin 1 : N Photo)

---
+++ src/main/java/org/konkuk/klab/mtot/repository/PinRepository.java
@@ -0,0 +1,20 @@
+package org.konkuk.klab.mtot.repository;
+
+import org.konkuk.klab.mtot.domain.Pin;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Optional;
+
+public interface PinRepository extends JpaRepository<Pin, Long> {
+
+    @Query("select p from Pin p " +
+            "where p.member.id =:memberId and " +
+            "p.journey.id =:journeyId " +
+            "order by p.createdTime desc " +
+            "limit 1")
+    Optional<Pin> findFirstPinByMemberIdAndJourneyId(@Param("memberId") Long memberId,
+                                                     @Param("journeyId") Long journeyId);
+
+}
