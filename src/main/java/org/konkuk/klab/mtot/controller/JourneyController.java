eafb677562f27d38eec6903dcecf851266b5bc74
algoORgoal <qufstar46@gmail.com>
2023-06-01 23:21:12 +0900

    fix: split PathVariables into @Requestbody and @LoginMemberEmail

--- src/main/java/org/konkuk/klab/mtot/controller/JourneyController.java
+++ src/main/java/org/konkuk/klab/mtot/controller/JourneyController.java
@@ -1,7 +1,11 @@
 package org.konkuk.klab.mtot.controller;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.konkuk.klab.mtot.dto.request.CreateJourneyRequest;
+import org.konkuk.klab.mtot.dto.request.MemberSignUpRequest;
 import org.konkuk.klab.mtot.dto.response.CreateJourneyResponse;
+import org.konkuk.klab.mtot.oauth.LoginMemberEmail;
 import org.konkuk.klab.mtot.service.JourneyService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -11,9 +15,9 @@ import org.springframework.web.bind.annotation.*;
 @RequiredArgsConstructor
 public class JourneyController {
     private final JourneyService journeyService;
-    @PostMapping("/members/{memberEmail}/journies/{journeyName}/teams/{teamId}")
-    public ResponseEntity<CreateJourneyResponse> createJourney(@PathVariable("memberEmail") String memberEmail, @PathVariable("journeyName") String journeyName, @PathVariable("teamId") Long teamId){
-        CreateJourneyResponse createJourneyResponse = journeyService.createJourney(memberEmail, journeyName, teamId);
+    @PostMapping("/teams/{teamId}")
+    public ResponseEntity<CreateJourneyResponse> createJourney(@LoginMemberEmail String email, @RequestBody @Valid CreateJourneyRequest createJourneyRequest){
+        CreateJourneyResponse createJourneyResponse = journeyService.createJourney(email, createJourneyRequest.getJourneyName(), createJourneyRequest.getTeamId());
         return ResponseEntity.ok(createJourneyResponse);
     }
 }
