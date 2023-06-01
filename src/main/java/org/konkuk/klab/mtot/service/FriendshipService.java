eef452501f09332cae92d0c39b48c16d10eb9175
GwonPyo <ngp9441@gmail.com>
2023-06-01 21:24:59 +0900

    modify all functions' arguments

--- src/main/java/org/konkuk/klab/mtot/service/FriendshipService.java
+++ src/main/java/org/konkuk/klab/mtot/service/FriendshipService.java
@@ -3,11 +3,9 @@ package org.konkuk.klab.mtot.service;
 import lombok.RequiredArgsConstructor;
 import org.konkuk.klab.mtot.domain.Friendship;
 import org.konkuk.klab.mtot.domain.Member;
-import org.konkuk.klab.mtot.dto.request.FriendCheckRequest;
-import org.konkuk.klab.mtot.dto.request.FriendshipCheckRequest;
-import org.konkuk.klab.mtot.dto.request.FriendshipRequest;
-import org.konkuk.klab.mtot.dto.request.FriendshipUpdateRequest;
 import org.konkuk.klab.mtot.dto.response.FriendshipResponse;
+import org.konkuk.klab.mtot.exception.DuplicateFriendshipException;
+import org.konkuk.klab.mtot.exception.FriendshipNotFoundException;
 import org.konkuk.klab.mtot.exception.MemberNotFoundException;
 import org.konkuk.klab.mtot.repository.FriendshipRepository;
 import org.konkuk.klab.mtot.repository.MemberRepository;
@@ -25,10 +23,10 @@ public class FriendshipService {
 
     @Transactional
     // 친구 요청 보내기 (친구 요청 DB에 저장)
-    public FriendshipResponse requestFriend(FriendshipRequest request){
-        Member requester = memberRepository.findByEmail(request.getRequesterEmail())
+    public FriendshipResponse requestFriend(String requesterEmail, String receiverEmail){
+        Member requester = memberRepository.findByEmail(requesterEmail)
                 .orElseThrow(MemberNotFoundException::new);
-        Member receiver = memberRepository.findByEmail(request.getReceiverEmail())
+        Member receiver = memberRepository.findByEmail(receiverEmail)
                 .orElseThrow(MemberNotFoundException::new);
         validateDuplicateFriendship(requester, receiver);
 
@@ -38,40 +36,41 @@ public class FriendshipService {
 
     @Transactional(readOnly = true)
     // 유저 친구 확인
-    public List<Friendship> checkMemberFriend(FriendCheckRequest request){
-        Member member = memberRepository.findByEmail(request.getMemberEmail()).orElseThrow(MemberNotFoundException::new);
+    public List<Friendship> checkMemberFriend(String memberEmail){
+        Member member = memberRepository.findByEmail(memberEmail).orElseThrow(MemberNotFoundException::new);
         List<Friendship> friendshipList = friendshipRepository.findMemberFriend(member.getId());
         return friendshipList;
     }
 
     @Transactional(readOnly = true)
     // 유저가 보낸 친구 요청 확인 (수락 되지 않은 것)
-    public List<Friendship> checkMemberRequestNotAccepted(FriendshipCheckRequest request){
-        Member member = memberRepository.findByEmail(request.getMemberEmail()).orElseThrow(MemberNotFoundException::new);
+    public List<Friendship> checkMemberRequestNotAccepted(String memberEmail){
+        Member member = memberRepository.findByEmail(memberEmail).orElseThrow(MemberNotFoundException::new);
         List<Friendship> friendshipList = friendshipRepository.findPendingFriendshipReceivedByMemberId(member.getId());
         return friendshipList;
     }
 
     @Transactional(readOnly = true)
     // 유저가 받은 친구 요청 확인 (수락 하지 않은 것)
-    public List<Friendship> checkMemberReceiveNotAccept(FriendshipCheckRequest request){
-        Member member = memberRepository.findByEmail(request.getMemberEmail()).orElseThrow(MemberNotFoundException::new);
-        List<Friendship> friendshipList = friendshipRepository.findPendingFrendshipRequestedByMemberId(member.getId());
+    public List<Friendship> checkMemberReceiveNotAccept(String memberEmail){
+        Member member = memberRepository.findByEmail(memberEmail).orElseThrow(MemberNotFoundException::new);
+        List<Friendship> friendshipList = friendshipRepository.findPendingFriendshipRequestedByMemberId(member.getId());
         return friendshipList;
     }
 
     @Transactional
     // 친구 추가 거절 or 수락 요청 처리 및 이후 친구 삭제 기능 시 사용 가능
-    public int updateFriendship(FriendshipUpdateRequest request){
+    public int updateFriendship(boolean isAccepted,String requesterEmail, String receiverEmail){
         int updatedRows; // 변환된 행 개수 반환 받음
-        Member requester = memberRepository.findByEmail(request.getRequesterEmail()).orElseThrow(MemberNotFoundException::new);
-        Member receiver = memberRepository.findByEmail(request.getReceiverEmail())
+        Member requester = memberRepository.findByEmail(requesterEmail)
+                .orElseThrow(MemberNotFoundException::new);
+        Member receiver = memberRepository.findByEmail(receiverEmail)
                 .orElseThrow(MemberNotFoundException::new);
         validateFriendshipExist(requester, receiver);
 
         Optional<Friendship> friendship = friendshipRepository.findByRequesterIdAndReceiverId(requester.getId(), receiver.getId());
 
-        if(request.getIsAccepted()){
+        if(isAccepted){
             friendship.get().setAccepted(true);
             updatedRows = 1;
         }
@@ -85,18 +84,18 @@ public class FriendshipService {
     private void validateDuplicateFriendship(Member Requester, Member Receiver){
         friendshipRepository.findByRequesterIdAndReceiverId(Requester.getId(), Receiver.getId())
                         .ifPresent(friendship -> {
-                            throw new RuntimeException("이미 신청되었거나 친구입니다.");
+                            throw new DuplicateFriendshipException();
                         });
 
         friendshipRepository.findByRequesterIdAndReceiverId(Receiver.getId(), Requester.getId())
                         .ifPresent(friendship -> {
-                            throw new RuntimeException("이미 신청되었거나 친구입니다.");
+                            throw new DuplicateFriendshipException();
                         });
     }
 
     // checkAccept, updateFriendship 함수 사용 전 실제로 해당 요청이 있었는지 확인
     private void validateFriendshipExist(Member Requester, Member Receiver){
         if(friendshipRepository.findByRequesterIdAndReceiverId(Requester.getId(), Receiver.getId()).isEmpty())
-            throw new RuntimeException("목표 friendship이 존재하지 않습니다.");
+            throw new FriendshipNotFoundException();
     }
 }
