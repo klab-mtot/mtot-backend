e02281fbf2f8cae8f93577dca5d16ef34c0132ff
algoORgoal <qufstar46@gmail.com>
2023-06-01 22:03:25 +0900

    feat: add a controller to Journey service

---
+++ src/main/java/org/konkuk/klab/mtot/controller/JourneyController.java
@@ -0,0 +1,27 @@
+package org.konkuk.klab.mtot.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.konkuk.klab.mtot.domain.Journey;
+import org.konkuk.klab.mtot.domain.Team;
+import org.konkuk.klab.mtot.dto.response.CreateJourneyResponse;
+import org.konkuk.klab.mtot.dto.response.MemberGetAllResponse;
+import org.konkuk.klab.mtot.exception.TeamAccessDeniedException;
+import org.konkuk.klab.mtot.exception.TeamNotFoundException;
+import org.konkuk.klab.mtot.service.JourneyService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Objects;
+
+@RequestMapping("/journey")
+@RestController
+@RequiredArgsConstructor
+public class JourneyController {
+    private final JourneyService journeyService;
+    @PostMapping("/members/{memberEmail}/journies/{journeyName}/teams/{teamId}")
+    public ResponseEntity<CreateJourneyResponse> createJourney(@PathVariable("memberEmail") String memberEmail, @PathVariable("journeyName") String journeyName, @PathVariable("teamId") Long teamId){
+        CreateJourneyResponse createJourneyResponse = journeyService.createJourney(memberEmail, journeyName, teamId);
+        return ResponseEntity.ok(createJourneyResponse);
+    }
+}
