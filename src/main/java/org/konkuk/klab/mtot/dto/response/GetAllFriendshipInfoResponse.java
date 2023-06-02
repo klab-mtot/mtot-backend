63350f8354d3d8f49eef4e15b3843b681e65eaec
donghoony <aru0504@naver.com>
2023-06-02 16:55:43 +0900

    feat: Add response & requests

--- src/main/java/org/konkuk/klab/mtot/dto/response/GetAllFriendshipInfoResponse.java
+++ src/main/java/org/konkuk/klab/mtot/dto/response/GetAllFriendshipInfoResponse.java
@@ -3,8 +3,10 @@ package org.konkuk.klab.mtot.dto.response;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
+import java.util.List;
+
 @Getter
 @AllArgsConstructor
-public class FriendshipUpdateResponse {
-    private boolean success;
+public class GetAllFriendshipInfoResponse {
+    List<FriendshipInfoResponse> friendShips;
 }
