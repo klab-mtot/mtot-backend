caf30e1c1eb750913e2b4e0050b0c243f2b87b65
Donghoon Lee <aru0504@naver.com>
2023-06-02 20:21:35 +0900

    Fix: Friendship 수정 (#28)
    * refactor: Request에 Constructor 작성
    
    * fix: url 수정
    
    * fix: Add Noargsconstructor
    
    * fix: 대소문자 수정

--- src/main/java/org/konkuk/klab/mtot/controller/FriendshipController.java
+++ src/main/java/org/konkuk/klab/mtot/controller/FriendshipController.java
@@ -1,10 +1,12 @@
 package org.konkuk.klab.mtot.controller;
 
 import lombok.RequiredArgsConstructor;
-import org.konkuk.klab.mtot.dto.request.FriendshipCheckRequest;
-import org.konkuk.klab.mtot.dto.request.FriendshipRequest;
 import org.konkuk.klab.mtot.dto.request.FriendshipAcceptRequest;
-import org.konkuk.klab.mtot.dto.response.*;
+import org.konkuk.klab.mtot.dto.request.FriendshipRequest;
+import org.konkuk.klab.mtot.dto.response.FriendshipUpdateResponse;
+import org.konkuk.klab.mtot.dto.response.GetAllFriendshipInfoResponse;
+import org.konkuk.klab.mtot.dto.response.GetAllPendingFriendshipResponse;
+import org.konkuk.klab.mtot.dto.response.SendFriendshipResponse;
 import org.konkuk.klab.mtot.oauth.LoginMemberEmail;
 import org.konkuk.klab.mtot.service.FriendshipService;
 import org.springframework.http.ResponseEntity;
@@ -30,15 +32,14 @@ public class FriendshipController {
         return ResponseEntity.ok(response);
     }
 
-    @GetMapping("/friendship/pending")
+    @GetMapping("/pending")
     @ResponseBody
-    public ResponseEntity<GetAllPendingFriendshipResponse> getPendingFriendship(@LoginMemberEmail String email,
-                                                                        @RequestBody FriendshipCheckRequest request){
+    public ResponseEntity<GetAllPendingFriendshipResponse> getPendingFriendship(@LoginMemberEmail String email){
         GetAllPendingFriendshipResponse response = friendshipService.getAllPendingFriendRequests(email);
         return ResponseEntity.ok(response);
     }
 
-    @PostMapping("/friendship/reject")
+    @PostMapping("/reject")
     @ResponseBody
     public ResponseEntity<FriendshipUpdateResponse> rejectFriendList(@LoginMemberEmail String email,
                                                                      @RequestBody FriendshipAcceptRequest request){
@@ -46,7 +47,7 @@ public class FriendshipController {
         return ResponseEntity.ok(response);
     }
 
-    @PostMapping("/friendship/accept")
+    @PostMapping("/accept")
     @ResponseBody
     public ResponseEntity<FriendshipUpdateResponse> acceptFriendList(@LoginMemberEmail String email,
                                                                      @RequestBody FriendshipAcceptRequest request){
--- src/main/java/org/konkuk/klab/mtot/dto/request/EditPostRequest.java
+++ src/main/java/org/konkuk/klab/mtot/dto/request/EditPostRequest.java
@@ -1,8 +1,12 @@
 package org.konkuk.klab.mtot.dto.request;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
+@NoArgsConstructor
+@AllArgsConstructor
 public class EditPostRequest {
     private Long journeyId;
     private String title;
