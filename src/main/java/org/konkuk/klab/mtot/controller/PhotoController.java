a0be416df8f022ce6d0cdae1d7af7dfbbd745bf2
donghoony <aru0504@naver.com>
2023-06-02 19:01:16 +0900

    refactor: Refactor photo service & controller

--- src/main/java/org/konkuk/klab/mtot/controller/PhotoController.java
+++ src/main/java/org/konkuk/klab/mtot/controller/PhotoController.java
@@ -1,55 +1,52 @@
 package org.konkuk.klab.mtot.controller;
 
-import jakarta.validation.Valid;
-import lombok.Getter;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
 import lombok.RequiredArgsConstructor;
-import org.konkuk.klab.mtot.domain.Journey;
-import org.konkuk.klab.mtot.domain.Pin;
+import org.konkuk.klab.mtot.dto.request.PhotoUploadRequest;
 import org.konkuk.klab.mtot.dto.response.CalenderThumbnailResponse;
-import org.konkuk.klab.mtot.dto.response.PhotoLinksResponse;
+import org.konkuk.klab.mtot.dto.response.GetAllPhotoUrlResponse;
 import org.konkuk.klab.mtot.dto.response.PhotoUploadResponse;
-import org.konkuk.klab.mtot.s3.AwsS3FileSupporter;
-import org.konkuk.klab.mtot.service.JourneyService;
+import org.konkuk.klab.mtot.oauth.LoginMemberEmail;
 import org.konkuk.klab.mtot.service.PhotoService;
-import org.konkuk.klab.mtot.service.PinService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
-import java.time.LocalDate;
-import java.util.HashMap;
-import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/photos")
+@RequestMapping("/photo")
 public class PhotoController {
+
     private final PhotoService photoService;
-    private final PinService pinService;
-    private final JourneyService journeyService;
+
     @GetMapping("/pin/{id}")
-    private ResponseEntity<PhotoLinksResponse> getLinkByPin(@PathVariable("id") Long pinId){
-        PhotoLinksResponse response = photoService.findPhotoByPin(pinService.findPinById(pinId));
+    private ResponseEntity<GetAllPhotoUrlResponse> getLinkByPin(@LoginMemberEmail String email,
+                                                                @PathVariable("id") Long pinId){
+        GetAllPhotoUrlResponse response = photoService.getAllPhotosUrlByPinId(email, pinId);
         return ResponseEntity.ok(response);
     }
 
-    @GetMapping("/Journey/{id}")
-    private ResponseEntity<PhotoLinksResponse> getLinkByJourney(@PathVariable("id") Long journeyId){
-        PhotoLinksResponse response = photoService.findPhotoByJourney(journeyService.findJourneyById(journeyId));
+    @GetMapping("/journey/{id}")
+    private ResponseEntity<GetAllPhotoUrlResponse> getLinkByJourney(@LoginMemberEmail String email,
+                                                                    @PathVariable("id") Long journeyId){
+        GetAllPhotoUrlResponse response = photoService.getAllPhotosByJourneyId(email, journeyId);
         return ResponseEntity.ok(response);
     }
 
     @GetMapping("/calenderThumbnail")
-    private ResponseEntity<CalenderThumbnailResponse> getCalenderThumbnailLink(
-            @RequestBody @Valid @RequestParam("year") int year, @RequestParam("month") int month){
-        CalenderThumbnailResponse response = photoService.findThumbnail(year,month);;
+    private ResponseEntity<CalenderThumbnailResponse> getCalenderThumbnailLink(@LoginMemberEmail String email,
+                                                                               @RequestParam(name = "year") int year,
+                                                                               @RequestParam(name = "month") @Min(1) @Max(12) int month){
+        CalenderThumbnailResponse response = photoService.findThumbnail(email, year, month);
         return ResponseEntity.ok(response);
     }
 
     @PostMapping
-    public ResponseEntity<PhotoUploadResponse> uploadPhotos2aws(@RequestBody @Valid  String loginEmail, Long pinId, List<MultipartFile> multipartFiles) throws IOException {
-        PhotoUploadResponse response = photoService.uploadPhotos(loginEmail,pinId,multipartFiles);
+    public ResponseEntity<PhotoUploadResponse> uploadPhotos2aws(@LoginMemberEmail String email,
+                                                                @RequestBody PhotoUploadRequest request) throws IOException {
+        PhotoUploadResponse response = photoService.uploadPhotos(email, request.getPinId(), request.getImages());
         return ResponseEntity.ok(response);
     }
 }
--- src/main/java/org/konkuk/klab/mtot/service/PhotoService.java
+++ src/main/java/org/konkuk/klab/mtot/service/PhotoService.java
@@ -2,15 +2,16 @@ package org.konkuk.klab.mtot.service;
 
 import lombok.RequiredArgsConstructor;
 import org.konkuk.klab.mtot.domain.Journey;
+import org.konkuk.klab.mtot.domain.Member;
 import org.konkuk.klab.mtot.domain.Photo;
 import org.konkuk.klab.mtot.domain.Pin;
-import org.konkuk.klab.mtot.dto.response.CalenderThumbnailResponse;
-import org.konkuk.klab.mtot.dto.response.PhotoLinksResponse;
-import org.konkuk.klab.mtot.dto.response.PhotoUploadResponse;
+import org.konkuk.klab.mtot.dto.response.*;
 import org.konkuk.klab.mtot.exception.JourneyNotFoundException;
+import org.konkuk.klab.mtot.exception.MemberNotFoundException;
 import org.konkuk.klab.mtot.exception.PinNotFound;
 import org.konkuk.klab.mtot.exception.TeamAccessDeniedException;
 import org.konkuk.klab.mtot.repository.JourneyRepository;
+import org.konkuk.klab.mtot.repository.MemberRepository;
 import org.konkuk.klab.mtot.repository.PhotoRepository;
 import org.konkuk.klab.mtot.repository.PinRepository;
 import org.konkuk.klab.mtot.s3.AwsS3FileSupporter;
@@ -21,7 +22,6 @@ import org.springframework.web.multipart.MultipartFile;
 import java.io.IOException;
 import java.time.LocalDate;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 
 @Service
@@ -31,7 +31,9 @@ public class PhotoService {
     private final AwsS3FileSupporter awsS3FileSupporter;
     private final PhotoRepository photoRepository;
     private final JourneyRepository journeyRepository;
-    public final PinRepository pinRepository;
+    private final PinRepository pinRepository;
+    private final MemberRepository memberRepository;
+
 
     @Transactional
     public PhotoUploadResponse uploadPhotos(String loginEmail, Long pinId, List<MultipartFile> multipartFiles) throws IOException {
@@ -60,34 +62,62 @@ public class PhotoService {
         return new PhotoUploadResponse(photoIds, imageUrls);
     }
 
-    public PhotoLinksResponse  findPhotoByPin(Pin pin){
-        List<Photo> photos = photoRepository.findByPin(pin);
-        List<String> links = new ArrayList();
-        for(Photo photo : photos){
-            links.add(photo.getFilePath());
-        }
-        return new PhotoLinksResponse(links);
+    @Transactional(readOnly = true)
+    public CalenderThumbnailResponse findThumbnail(String email, int year, int month){
+        Member member = memberRepository.findByEmail(email).orElseThrow(MemberNotFoundException::new);
+
+        LocalDate startDate = LocalDate.of(year, month,1);
+        LocalDate endDate = startDate.withDayOfMonth(startDate.lengthOfMonth());
+        
+        // TODO: Member가 가진 Journey 가져오기 필요
+        List<DayThumbnailResponse> dayThumbnailResponses = photoRepository.getThumbnailPhotosBetween(startDate, endDate)
+                .stream()
+                .map(photo -> new DayThumbnailResponse(photo.getUploadDate().getDayOfMonth(), photo.getImageUrl()))
+                .toList();
+        return new CalenderThumbnailResponse(dayThumbnailResponses);
     }
 
-    public PhotoLinksResponse  findPhotoByJourney(Journey journey){
-        List<Photo> photos = photoRepository.findByJourney(journey);
-        List<String> links = new ArrayList();
-        for(Photo photo : photos){
-            links.add(photo.getFilePath());
-        }
-        return new PhotoLinksResponse(links);
+    @Transactional(readOnly = true)
+    public GetAllPhotoUrlResponse getAllPhotosUrlByPinId(String email, Long pinId) {
+
+        Member member = memberRepository.findByEmail(email).orElseThrow(MemberNotFoundException::new);
+        Pin pin = pinRepository.findById(pinId).orElseThrow(PinNotFound::new);
+        pin.getJourney().getTeam().getMemberTeams()
+                .stream()
+                .filter(memberTeam -> memberTeam.getMember().getId().equals(member.getId()))
+                .findAny()
+                .orElseThrow(TeamAccessDeniedException::new);
+
+        List<PhotoUrlResponse> photoUrlResponses = photoRepository.findAllByPinId(pinId)
+                .stream()
+                .map(photo -> {
+                    return new PhotoUrlResponse(photo.getImageUrl());
+                })
+                .toList();
+
+        return new GetAllPhotoUrlResponse(photoUrlResponses);
     }
 
-    public CalenderThumbnailResponse findThumbnail(int year, int month){
-        LocalDate start = LocalDate.of(year,month,1);
-        LocalDate end = start.withDayOfMonth(start.lengthOfMonth());
-        List<Photo> photos = photoRepository.findByMonth(start,end);
-        HashMap<LocalDate,String> map = new HashMap<>();
-        for(Photo photo:photos){
-            if (!map.containsKey(photo.getUploadDate())){
-                map.put(photo.getUploadDate(),photo.getFilePath());
-            }
-        }
-        return new CalenderThumbnailResponse(map);
+    @Transactional(readOnly = true)
+    public GetAllPhotoUrlResponse getAllPhotosByJourneyId(String email, Long journeyId) {
+        Member member = memberRepository.findByEmail(email).orElseThrow(MemberNotFoundException::new);
+
+        Journey journey = journeyRepository.findById(journeyId).orElseThrow(JourneyNotFoundException::new);
+
+        journey.getTeam().getMemberTeams()
+                .stream()
+                .filter(memberTeam -> memberTeam.getMember().getId().equals(member.getId()))
+                .findAny()
+                .orElseThrow(TeamAccessDeniedException::new);
+
+        List<PhotoUrlResponse> photoUrlResponses = photoRepository.findByJourneyId(journey.getId())
+                .stream()
+                .map(photo -> {
+                    return new PhotoUrlResponse(photo.getImageUrl());
+                })
+                .toList();
+
+        return new GetAllPhotoUrlResponse(photoUrlResponses);
     }
+
 }
