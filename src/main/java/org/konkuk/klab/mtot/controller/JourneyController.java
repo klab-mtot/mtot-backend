e32bc08d38dc116108c20dce11c16c2766d00854
algoORgoal <qufstar46@gmail.com>
2023-06-02 20:21:18 +0900

    feat: add controllers for journey

--- src/main/java/org/konkuk/klab/mtot/controller/JourneyController.java
+++ src/main/java/org/konkuk/klab/mtot/controller/JourneyController.java
@@ -4,6 +4,8 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.konkuk.klab.mtot.dto.request.CreateJourneyRequest;
 import org.konkuk.klab.mtot.dto.response.CreateJourneyResponse;
+import org.konkuk.klab.mtot.dto.response.GetJourneyListResponse;
+import org.konkuk.klab.mtot.dto.response.GetJourneyResponse;
 import org.konkuk.klab.mtot.oauth.LoginMemberEmail;
 import org.konkuk.klab.mtot.service.JourneyService;
 import org.springframework.http.ResponseEntity;
@@ -14,6 +16,20 @@ import org.springframework.web.bind.annotation.*;
 @RequiredArgsConstructor
 public class JourneyController {
     private final JourneyService journeyService;
+
+    @GetMapping("/{journeyId}")
+    public ResponseEntity<GetJourneyResponse> getJourney(@LoginMemberEmail String email,
+                                                         @PathVariable("journeyId") Long journeyId){
+        GetJourneyResponse getJourneyResponse = journeyService.getJourney(email, journeyId);
+        return ResponseEntity.ok(getJourneyResponse);
+    }
+
+    @GetMapping
+    public ResponseEntity<GetJourneyListResponse> getJourneyList(@LoginMemberEmail String email){
+        GetJourneyListResponse getJourneyListResponse = journeyService.getJourneyList(email);
+        return ResponseEntity.ok(getJourneyListResponse);
+    }
+
     @PostMapping
     public ResponseEntity<CreateJourneyResponse> createJourney(@LoginMemberEmail String email, @RequestBody @Valid CreateJourneyRequest createJourneyRequest){
         CreateJourneyResponse createJourneyResponse = journeyService.createJourney(email, createJourneyRequest.getJourneyName(), createJourneyRequest.getTeamId());
