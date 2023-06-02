a8a1a6c1062815d9845e6c51ed8a466116c006ef
algoORgoal <qufstar46@gmail.com>
2023-06-03 06:00:17 +0900

    fix: JPA Query (=:)

--- src/main/java/org/konkuk/klab/mtot/repository/JourneyRepository.java
+++ src/main/java/org/konkuk/klab/mtot/repository/JourneyRepository.java
@@ -9,7 +9,7 @@ import java.util.List;
 
 public interface JourneyRepository extends JpaRepository<Journey, Long> {
 
-    @Query("select journey from Journey journey join journey.team team join team.memberTeams memberTeam where memberTeam.member.id =: memberId")
+    @Query("select journey from Journey journey join journey.team team join team.memberTeams memberTeam where memberTeam.member.id =:memberId")
     List<Journey> getJourneysFromMember(@Param("memberId") Long memberId);
 
 }
