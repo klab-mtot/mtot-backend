4907016b7293f845046fd1d6f173a0dc88b4885a
donghoony <aru0504@naver.com>
2023-06-03 18:21:16 +0900

    refactor: memberTeamList -> teamList 필드 수정

--- src/main/java/org/konkuk/klab/mtot/dto/response/MemberTeamGetAllResponse.java
+++ src/main/java/org/konkuk/klab/mtot/dto/response/MemberTeamGetAllResponse.java
@@ -8,5 +8,5 @@ import java.util.List;
 @Getter
 @AllArgsConstructor
 public class MemberTeamGetAllResponse {
-    List<MemberTeamGetResponse> memberTeamList;
+    List<MemberTeamGetResponse> teamList;
 }
